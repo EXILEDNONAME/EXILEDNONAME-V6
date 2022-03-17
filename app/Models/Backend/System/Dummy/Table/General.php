43a23832a3fad08178fc38bf8bb1a7e4e2be1934
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-03-17 11:56:33 +0700

    Update Page

--- app/Models/Backend/System/Dummy/Table/General.php
+++ app/Models/Backend/System/Dummy/Table/General.php
@@ -3,8 +3,11 @@
 namespace App\Models\Backend\System\Dummy\Table;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class General extends Model {
+
+  use LogsActivity;
 
   protected $table = 'dummy_table_generals';
   protected $primaryKey = 'id';
